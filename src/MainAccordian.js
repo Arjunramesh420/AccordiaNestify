b7c52f52e5c8df5d509fc9b3123708b5388df3a3
ArjunRamesh <arjunramesh2001@gmail.com>
2023-11-29 13:30:11 +0530

    new commit

--- src/MainAccordian.js
+++ src/MainAccordian.js
@@ -1,26 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
+import SubAccordion from './SubAccordian';
 
+import axios from 'axios';
 
+export default function MainAccordion({ title, subAccordion }) {
+  const mainAccordionStyles = {
+    backgroundColor: 'gray',
+  };
 
-export default function SubAccordian({subTitle,chips}) {
+  const titleStyles = {
+    color: 'white',
+    fontWeight: 'bold',
+  };
+
+  const [industries, setIndustries] = useState([]);
+
+  useEffect(() => {
+    // Fetch industry data from the backend API
+    axios.get('https://app.gigleji.com/api/industry/')
+      .then(response => {
+        setIndustries(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching industry data:', error);
+      });
+  }, []);
+
   return (
-    
-    <Accordion>
-      <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel-content" id="panel-header">
-        <Typography variant="subtitle1">{subTitle}</Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-      <Stack direction="row" spacing={1} display={'flex'} flexWrap={'wrap'}>
-        {chips?.map((chip,index)=>(
-            <Chip key={index} label={chip} style={{ margin: '5px' }} />
-        ))}
-    </Stack>
-      </AccordionDetails>
-    </Accordion>
-    
-  )
+    <div>
+      {industries.map((industry, index) => (
+        <Accordion key={index} style={mainAccordionStyles} square={true}>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel-content" id="panel-header">
+            <Typography variant="h6" style={titleStyles}>
+              {industry.title}
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            {industry.domains?.map((domain, domainIndex) => (
+              <SubAccordion key={domainIndex} subTitle={domain.Domain} chips={domain.skillset} />
+            ))}
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </div>
+  );
 }
